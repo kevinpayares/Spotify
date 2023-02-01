4fbcac79e857ac6c4061de7b7ca79924ca78848c
DOMAIN\kevin.payares <kpayares@intergrupo.com>
2023-02-01 11:07:01 -0500

    Se crean los modulos iniciales con sus rutas y el primer componente de home

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from '@shared//components/side-bar/side-bar.component';
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent, 
+    SideBarComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
