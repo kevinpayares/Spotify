ad0fc2b00cf9bd83e9666e6dfedf0655dfde400c
DESKTOP-BLK6U79\Kevin Payares <kevinpayares1@gmail.com>
2023-02-01 21:31:09 -0500

    Agregamos contenido dinamico al componente slidebar con logica en el .ts y agregamos icono y nombre en el html de dicho componente

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
 
+export class SideBarComponent implements OnInit {
+
+  mainMenu: {
+    defaultOptions: Array<any>,
+    accessLink: Array<any>,
+  } = { defaultOptions: [], accessLink: [] };
+
+  customOptions: Array<any> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil-estate',
+        route: ['/', 'history']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil-search',
+        route: ['', 'favorites']
+      },
+      {
+        name: 'Tu Biblioteca',
+        icon: 'uil-chart',
+        route: ['', '']
+      }
+    ];
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square',
+        route: ''
+      },
+      {
+        name: 'Canciones que me gustan',
+        icon: 'uil-star',
+        route: ''
+      }
+    ];
+
+    this.customOptions = [
+      {
+        
+        name: 'Lista No. 1',
+        icon: 'uil-music',
+        route: ''
+      },
+      {
+        name: 'Lista No. 4',
+        icon: 'uil-music',
+        route: ''
+      },
+      {
+        name: 'Lista No. 3',
+        icon: 'uil-music',
+        route: ''
+      },
+    ]
+  }
 }
